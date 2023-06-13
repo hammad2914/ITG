70566e6f281c891ac48dd86c3f5b5dd4665119ab
Hammad Ali <ch.hammad2914@gmail.com>
2023-06-13 12:22:58 +0500

    Update 5

---
+++ js/screenChange.js
@@ -0,0 +1,16 @@
+function goToSourcing() {
+    window.location.href = "/sub-links/sourcing.html";
+  }
+
+function goToSustainability(){
+    window.location.href = "/sub-links/sustainability.html";
+}  
+
+function goToSupplyChain(){
+    window.location.href = "/sub-links/supply-chain.html";
+}  
+
+function goToPage(url) {
+    window.location.href = url;
+  }
+  
